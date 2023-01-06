84e815c09d34017bfa09e1c7b3dbacd535a571fc
Shaikh Saad <114829613+saadmd29@users.noreply.github.com>
2023-01-06 11:22:27 +0530

    Update main.tf

--- live/skillsit-s3-website/main.tf
+++ live/skillsit-s3-website/main.tf
@@ -3,10 +3,9 @@ provider "aws" {}
 module "s3-website" {
   source = "../../modules/s3-website"
 
-  bucket_name = "skillsit-s3-website"
+  bucket_name = "cloudpals-bucket"
 }
 
 output "website_endpoint" {
   value = module.s3-website.website_endpoint
 }
-
